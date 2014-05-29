49c5c783e04c582daefaf8018e34cd7c33ec630c
AtuyL <atuyl.jp@gmail.com>
2014-05-29 17:28:55 +0900

    added: route params support.

--- test/locale/getLocaleUrl.js
+++ test/locale/getLocaleUrl.js
@@ -7,7 +7,7 @@ var Lab = require("lab"),
 
 Lab.test("Getting a proper locale url", function (done) {
     var parts = url.parse("http://google.com:8080/monster"),
-        result = getLocaleUrl(parts, "/money", "de");
+        result = getLocaleUrl(parts, null, "/money", "de");
     expect(result).to.equal("http://google.com:8080/de/money");
     expect(parts.pathname).to.equal("/monster");
     done();
@@ -15,8 +15,16 @@ Lab.test("Getting a proper locale url", function (done) {
 
 Lab.test("Getting non-locale url by passing ''", function (done) {
     var parts = url.parse("http://google.com:8080/de/monster"),
-        result = getLocaleUrl(parts, "/money", "");
+        result = getLocaleUrl(parts, null, "/money", "");
     expect(result).to.equal("http://google.com:8080/money");
     expect(parts.pathname).to.equal("/de/monster");
+    done();
+});
+
+Lab.test("Param test", function (done) {
+    var parts = url.parse("http://google.com:8080/de/monster/alice/profile"),
+        result = getLocaleUrl(parts, {name: 'alice', action: 'profile'}, "/money/:name/:action", "");
+    expect(result).to.equal("http://google.com:8080/money/alice/profile");
+    expect(parts.pathname).to.equal("/de/monster/alice/profile");
     done();
 });
